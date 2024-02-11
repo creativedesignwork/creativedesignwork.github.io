21f3e0aa5b0e214407f796ff3752034615d49b21
creativedesignwork <159251973+creativedesignwork@users.noreply.github.com>
2024-02-11 14:44:37 +0530

    Base

---
+++ js/custom.js
@@ -0,0 +1,93 @@
+  // Get the modal
+  var modal = document.getElementById("myModal");
+
+  // Get the button that opens the modal
+  var btn = document.getElementById("myBtn");
+  
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+  
+  // When the user clicks on the button, open the modal
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+  
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
+  //back key press modal is showing
+  var showModal = function() {
+    // some code here to show the HTML elements...
+
+    window.history.pushState('backPressed', null, null);
+    window.history.pushState('dummy', null, null);
+    window.addEventListener('popstate', hideModal, { once: true });
+};
+
+var hideModal = function(event) {
+    if (event.state == 'backPressed') {
+        // hide the HTML elements
+    }
+};
+
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+// automatic slider
+onload  = start;
+
+function start(){	
+var i = 1;
+function Move(){ 	
+	i = (i%4)+1; // 4 is the Number of image in slider
+	document.getElementById('i'+i).checked = true;
+}
+setInterval(Move,5000); //change img in 3 sec
+}
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    content.style.display = "none";
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+$(document).ready(function(){
+  $('#videoElementID').bind('contextmenu',function() { return false; });
+});
