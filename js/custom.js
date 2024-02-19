9f9f70650777b3a5e3ce2ba5975a39424ed1d022
creativedesignswork <creativedesignswork2023@gmail.com>
2024-02-19 15:30:11 +0530

    update

--- js/custom.js
+++ js/custom.js
@@ -79,7 +79,7 @@ for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
-    content.style.display = "none";
+  //  content.style.display = "none";
     if (content.style.display === "block") {
       content.style.display = "none";
     } else {
